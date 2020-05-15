9dfab0d7b262d6ac609207cd2ebb3ac8cef4f11a
Piotr Dubiel <piotr.dubiel@polidea.com>
2020-05-15 22:33:50 +0200

    Make Blemulator module inherit from RCTEventEmitter

--- ios/Blemulator.m
+++ ios/Blemulator.m
@@ -3,12 +3,24 @@
 
 @implementation Blemulator
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
+
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"MethodCall"];
+}
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    [self sendEventWithName:@"MethodCall" body:@{@"name": stringArgument}];
 }
+
+RCT_EXPORT_METHOD(runTest)
+{
+    [self sendEventWithName:@"MethodCall" body:@{@"name": @"runTest"}];
+}
+
 
 @end
